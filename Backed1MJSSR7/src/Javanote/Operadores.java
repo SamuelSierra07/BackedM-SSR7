d2667039d134b47af1a48ce932e10460fd69e6b2
SamuelSierra07 <sierrasamuel80@gmail.com>
2024-08-22 07:36:36 -0500

    Condicionales
    clase 22 agosto

---
+++ Backed1MJSSR7/src/Javanote/Operadores.java
@@ -0,0 +1,25 @@
+package Javanote;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        int rest = 0;
+        int multi = 25;
+        int div = 14;
+
+        System.out.println("Resta = " + rest );
+        System.out.println("Multiplicacion =" + multi);
+        System.out.println("Divison = " + div);
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
