d333fa5baeee911f08dbaf9b29c7897eccb54f10
SamuelSierra07 <sierrasamuel80@gmail.com>
2024-08-29 07:27:21 -0500

    agosto29
    agosto29

--- Backed1MJSSR7/src/Javanote/Switch.java
+++ Backed1MJSSR7/src/Javanote/Switch.java
@@ -1,19 +1,82 @@
 package Javanote;
 
+import java.util.Scanner;
+
 public class Switch {
 
+    static int productId;
+    static String productName;
+    static double cost;
+    static float profit;
+    static double price;
 
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            menu();
+            System.out.println("¿Desea continuar? (si/no)");
+            String response = sc.nextLine();
+            if (response.equalsIgnoreCase("no")) {
+                running = false;
+            }
+        }
+    }
 
+    public static void createProduct() {
+        System.out.println("Id:");
+        productId = sc.nextInt();
+        sc.nextLine(); // Consume newline left-over
+        System.out.println("Nombre del producto:");
+        productName = sc.nextLine();
+        System.out.println("Costo:");
+        cost = sc.nextDouble();
+        System.out.println("Beneficio (%):");
+        profit = sc.nextFloat();
+        sc.nextLine(); // Consume newline left-over
+        price = calculatePrice(profit, cost);
+    }
 
+    public static double calculatePrice(float profit, double cost) {
+        return cost / (1 - (profit / 100));
+    }
 
+    public static void printProduct() {
+        System.out.println("ID: " + productId);
+        System.out.println("Nombre del producto: " + productName);
+        System.out.println("Costo: " + cost);
+        System.out.println("Beneficio: " + profit + "%");
+        System.out.println("Precio de venta: " + price);
+    }
 
+    public static void menu() {
+        System.out.println("Seleccione una opción:");
+        System.out.println("1. Crear Producto");
+        System.out.println("2. Ver Producto");
 
-
-
-
-
-
-
-
+        int opc = sc.nextInt();
+        sc.nextLine(); // Consume newline left-over
+        switch (opc) {
+            case 1:
+                System.out.println("Crear Producto:");
+                createProduct();
+                break;
+            case 2:
+                System.out.println("Ver Producto:");
+                printProduct();
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
--- Backed1MJSSR7/src/Javanote/VoidFunction.java
+++ Backed1MJSSR7/src/Javanote/VoidFunction.java
@@ -41,34 +41,18 @@ public class VoidFunction {
         password = sc.nextLine();
         System.out.println("Seleccione un estado: ");
         int select = sc.nextInt();
-        status = selectStatus(selection)
+
 
     }
     public static void printCostumerData(){
 
         System.out.println("id "+ costumerId + "\n"+
                 "Nombre" + costumerName + "\n" +
-                "Correo"+ email+"\n" +
-                "static" + );
+                "Correo"+ email+"\n" );
 
 
     }
 
-
-    public static int selectStatus(int selection){
-
-        if (selection == 1) {
-            String active = String.valueOf(StaticCostumer.TRUE);
-
-            return active;
-
-        }else if(selection == 2){
-            String inactive = String.valueOf(StaticCostumer.FALSE);
-        }else{
-            System.out.println("Seleccion no valida");
-        }
-
-        return 
 
 
 
