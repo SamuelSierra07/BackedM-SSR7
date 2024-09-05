e7087058791517705b121d5353d8a2ac7bf17b82
JhoanM32 <martinezgarciajhoanmanuel@gmail.com>
2024-09-05 07:44:42 -0500

    agosto 5
    creación de clases

---
+++ Backed1MJSSR7/src/Javanote/Costumer.java
@@ -0,0 +1,54 @@
+package Javanote;
+
+public class Costumer extends   Usuario {
+
+
+    //Atributos
+
+
+    String costumerTyoe;
+    int experenciePoint;
+
+
+
+
+
+    //Metodos
+
+
+    @Override
+    public void createUser() {
+        super.createUser();
+    }
+
+    @Override
+    public void delectUser(int userId) {
+        super.delectUser(userId);
+    }
+
+
+    @Override
+    public void selectUser() {
+        super.selectUser();
+    }
+
+    @Override
+    public void selectUserById(int userId) {
+        super.selectUserById(userId);
+    }
+
+    @Override
+    public void uptadeUser() {
+        super.uptadeUser();
+    }
+
+
+
+
+
+
+
+
+}
+
+
---
+++ Backed1MJSSR7/src/Javanote/Usuario.java
@@ -0,0 +1,58 @@
+package Javanote;
+
+import java.util.Scanner;
+
+public class Usuario {
+
+
+
+    int userId;
+    String UserName;
+    String LastName;
+    String email;
+    String password;
+    boolean status;
+
+
+    Scanner sc = new Scanner(System.in);
+    
+
+
+
+    //Metodos
+
+    public void createUser(){
+
+    }
+    public void selectUser(){
+
+    }
+
+    public void selectUserById(int userId){
+
+    }
+
+    public void uptadeUser(){
+
+    }
+
+    public void delectUser(int userId){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
